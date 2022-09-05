7c457413c0f113120db15f9059520bbc68edb86f
bhavin4397 <bhavinpatidar4397@gmail.com>
2022-09-05 20:23:45 +0530

    update

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,13 @@
 import React, {Component, useState} from "react";
 import '../styles/App.css';
 
+
 const App = () => {
   return (
-    <div id="main"></div>
+    <div id="main">
+      <h1 data-ns-test="project-name">Portfolio website</h1>
+      <p data-ns-test="project-description">i like coding</p>
+    </div>
   )
 }
 
